c882ca9d66f261e29c0210406a4e259649bbe530
Gustavo Adolfo González Hernández <guagonzalezhe@unal.edu.co>
2022-07-02 21:57:07 -0500

    final
    final

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -7,73 +7,57 @@ Módulo de entrenamiento del modelo.
     salvelo en models/precios-diarios.pkl
 """
 
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.neural_network import MLPRegressor
-import pickle as pkl
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+import joblib
+import os
 
-def load_data():
-    df = pd.read_csv('data_lake/business/features/precios-diarios.csv')
-    data = df["precio"]
-    return data
-    
-            # Se remueve la tendencia
-def tendencia_removida():
-    data = load_data()
-    data_d1 = [data[t] - data[t - 1] for t in range(1, len(data))]
-    return data_d1
-
-def comp_ciclica_removida():
-    data_d1 = tendencia_removida()
-    data_d1d12 = [data_d1[t] - data_d1[t - 12] for t in range(12, len(data_d1))]
-    return data_d1d12
-
-def valores_escalados():
-    data_d1d12 = comp_ciclica_removida()
-    scaler = MinMaxScaler()
-    data_d1d12_scaled = scaler.fit_transform(np.array(data_d1d12).reshape(-1, 1))
-    data_d1d12_scaled = [u[0] for u in data_d1d12_scaled] # el largo de los datos escalados es 9404
-    return data_d1d12_scaled
-    
-def matriz_regresores():
-    data_d1d12_scaled = valores_escalados()
-    P = 13
-    X = []
-    for t in range(P - 1, len(data_d1d12_scaled) - 1):
-        X.append([data_d1d12_scaled[t - n] for n in range(P)])
-    d = data_d1d12_scaled[P:] # el largo de X y d es 9391
-    return X
-
-def save_model(model):
-
-    with open("src/models/precios-diarios.pkl", "wb") as file:
-        pkl.dump(model, file, pkl.HIGHEST_PROTOCOL)
-
+import pandas as pd
 def train_daily_model():
-    X = matriz_regresores()
-    data_d1d12_scaled = valores_escalados()
-    H = 4  # Se escoge arbitrariamente
-    np.random.seed(123456)
-    mlp = MLPRegressor(
-        hidden_layer_sizes=(H,),
-        activation="logistic",
-        learning_rate="adaptive",
-        momentum=0.0,
-        learning_rate_init=0.002,
-        max_iter=100000,
-    )
-    model = mlp.fit(X[0:8441], data_d1d12_scaled[0:8441]) # 9391 - 950 = 8441. 9391 es el largo de X 
-                                                          #  y 950 es aproximadamente el 10% de los datos
-    save_model(model)
-train_daily_model()
-    
-    #raise NotImplementedError("Implementar esta función")
+
+    df = pd.read_csv("./data_lake/business/features/precios_diarios.csv")
+
+    df = df.drop(columns=["fecha"])
+
+    y = df["precio"].array
+    X = df.drop(columns=["precio"])
+    X_train, X_Rem, y_true_train, y_Rem = train_test_split(X, y, test_size=0.25, random_state=123)
+    X_valid, X_test, y_valid, y_true_test = train_test_split(X_Rem,
+                                                         y_Rem,
+                                                         test_size=0.3
+                                                         ,random_state=123)
+
+    X_test["y"] = y_true_test 
+    X_test.to_csv('src/models/datosforecast.csv')
+    model = MLPRegressor(max_iter= 150,
+        activation='relu', 
+        solver= 'sgd', 
+        learning_rate_init= 0.001) 
+
+    model = MLPRegressor() 
+   
+    param_grid = [
+          {
+              "hidden_layer_sizes": [(1,),(2,),(3,),(4,),(5,)], 
+              "random_state": [1000, 1001, 1002, 1003, 1004, 1005]
+              
+          }
+    ]
+
+    grid_search = GridSearchCV(estimator = model, 
+                          param_grid=param_grid, cv=5) 
+
+    dataTrain = grid_search.fit(X_train, y_true_train)
+
+    parent_dir = "src/models"
+    cwd = os.getcwd()
+    path_parent_dir = os.path.join(cwd, parent_dir)
+
+    joblib.dump(dataTrain, path_parent_dir + '/precios-diarios.pkl')
 
 
 if __name__ == "__main__":
     import doctest
-
+    train_daily_model()
     doctest.testmod()
-
-#train_daily_model()
