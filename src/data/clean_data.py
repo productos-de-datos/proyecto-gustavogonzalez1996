9df2a3cac5669f6629cfe418fef48c436f080872
Gustavo Adolfo González Hernández <guagonzalezhe@unal.edu.co>
2022-07-04 12:29:18 -0500

    Final clean data
    Clean data

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,10 +1,11 @@
+""" Limpia y transforma los archivos para consolidarlos y finalmente procesarlos
 """
-Módulo de limpieza de datos.
--------------------------------------------------------------------------------
+import pandas as pd
+def clean_data():
+    """Realice la limpieza y transformación de los archivos CSV.
 
-    Realice la limpieza y transformación de los archivos CSV.
-
-    Usando los archivos data_lake/raw/*.csv, cree el archivo data_lake/cleansed/precios-horarios.csv.
+    Usando los archivos data_lake/raw/*.csv,
+    cree el archivo data_lake/cleansed/precios-horarios.csv.
     Las columnas de este archivo son:
 
     * fecha: fecha en formato YYYY-MM-DD
@@ -13,41 +14,36 @@ Módulo de limpieza de datos.
 
     Este archivo contiene toda la información del 1997 a 2021.
 
-"""
+    """
+    horas = {'0': '00', '1':'01', '2':'02', '3':'03',
+     '4': '04', '5':'05', '6':'06', '7':'07', '8':'08', '9': '09'}
+    try:
+        datos = pd.read_csv("./data_lake/raw/1995.csv")
+    except FileNotFoundError:
+        datos= pd.read_csv("../../data_lake/raw/1995.csv")
 
-import pandas as pd
-import glob
-import os
+    datos = datos.rename(columns=horas)
+    datos = pd.melt(datos, id_vars= ["Fecha"], value_vars = [str(hour) if hour >=10
+    else '0'+ str(hour) for hour in range(0,24)])
 
-def clean_data():
-    
-    # merging the files
-    files_joined = os.path.join('data_lake/raw/', "*.csv")
+    for i in range(1996,2022):
+        route_try = True
+        try:
+            datos1 = pd.read_csv("./data_lake/raw/" + str(i) + ".csv")
+        except FileNotFoundError:
+            route_try = False
+            datos1 = pd.read_csv("../../data_lake/raw/" + str(i) + ".csv")
+        datos1 = datos1.rename(columns=horas)
+        datos1 = pd.melt(datos1, id_vars= ["Fecha"], value_vars = [str(hour) if hour >=10
+        else '0'+ str(hour) for hour in range(0,24)])
+        datos = pd.concat([datos,datos1])
 
-    # Return a list of all joined files
-    list_files = glob.glob(files_joined)
-
-    # Merge files by joining all files
-    Unionarchivos = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
-
-    Unionarchivos['fecha'] = pd.to_datetime(Unionarchivos['fecha'], format="%Y/%m/%d")
-
-    buscarnulos_media = Unionarchivos.where(Unionarchivos.notna(), Unionarchivos.mean(axis=1, numeric_only=True), axis=0)
- 
-    eliminar_duplicados = buscarnulos_media.drop_duplicates(['fecha'], keep='first')
-
-    convertirfilas_columnas = pd.melt(eliminar_duplicados, id_vars="fecha")
-    ordenar_valores= convertirfilas_columnas.sort_values(by = ['fecha', 'variable'])
-    ordenar_valores.rename(columns={'variable': 'hora', 
-                           'value': 'precio'}, inplace=True)
-
-    ordenar_valores.to_csv('data_lake/cleansed/precios-horarios.csv', encoding='utf-8', index=False)
-
-    #raise NotImplementedError("Implementar esta función")
+    datos =  datos.rename(columns={"Fecha": "fecha", "variable": "hora", "value": "precio"})
+    route = ("./data_lake/cleansed/precios-horarios.csv" if route_try
+            else "../../data_lake/cleansed/precios-horarios.csv")
+    datos.to_csv(route, index=False)
 
 if __name__ == "__main__":
-
     import doctest
-    doctest.testmod()
-
     clean_data()
+    doctest.testmod()
