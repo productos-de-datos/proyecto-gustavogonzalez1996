fdefa3e86ad80062b3225d43a2226f46b33316c4
Gustavo Adolfo González Hernández <guagonzalezhe@unal.edu.co>
2022-07-04 10:44:29 -0500

    Cambios3
    Cambios

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,53 +1,49 @@
+"""Lea con la libreria glob todos los CSV en la carpeta data_lake/raw/ los une con append y finalmente los convierte
+a pandas, elimina las filas vacias y posteriormente con la funcion de pandas melt organiza los datos
+con columnas fecha, hora y precio. organiza su formato, filtra desde 1997 a 2021 y exporta en csv el archivo procesado en data_lake/cleansed/
 """
-Módulo de limpieza de datos.
--------------------------------------------------------------------------------
 
-    Realice la limpieza y transformación de los archivos CSV.
 
+def clean_data():
+    """Realice la limpieza y transformación de los archivos CSV.
     Usando los archivos data_lake/raw/*.csv, cree el archivo data_lake/cleansed/precios-horarios.csv.
     Las columnas de este archivo son:
-
     * fecha: fecha en formato YYYY-MM-DD
     * hora: hora en formato HH
     * precio: precio de la electricidad en la bolsa nacional
-
     Este archivo contiene toda la información del 1997 a 2021.
+    """
+    import pandas as pd
+    import glob
+    import numpy as np
 
-"""
+    ruta=glob.glob(r'data_lake/raw/*.csv')
 
-import pandas as pd
-import glob
-import os
+    lista=[]
 
-def clean_data():
+    for archivo in ruta:
+        df=pd.read_csv(archivo,index_col=None,header=0)
+        lista.append(df)
     
-    # merging the files
-    files_joined = os.path.join('data_lake/raw/', "*.csv")
+    archivo_completo=pd.concat(lista,axis=0,ignore_index=True)
 
-    # Return a list of all joined files
-    list_files = glob.glob(files_joined)
+    archivo_completo=archivo_completo[archivo_completo["Fecha"].notnull()]
+    archivo_completo=pd.melt(archivo_completo,id_vars=['Fecha'],var_name='hora', value_name='precio')
+    archivo_completo['hora']=np.where(pd.to_numeric(archivo_completo['hora']) <= 9,pd.concat(["0"+archivo_completo['hora']]),archivo_completo['hora'])
+    archivo_completo["Fecha"] = pd.to_datetime(archivo_completo["Fecha"]).dt.strftime('%Y-%m-%d')
+    date1="2017-01-01"
+    date2 = "2021-12-31"
+    month_list = [i.strftime('%Y-%m-%d') for i in pd.date_range(start=date1, end=date2)]
+    archivo_completo=archivo_completo[archivo_completo.Fecha.isin(month_list)]
+    archivo_completo=archivo_completo.rename(columns={"Fecha":"fecha"})
+    archivo_completo.to_csv("data_lake/cleansed/precios-horarios.csv",index=False, header=True)
 
-    # Merge files by joining all files
-    Unionarchivos = pd.concat(map(pd.read_csv, list_files), ignore_index=True)
+    return
 
-    Unionarchivos['fecha'] = pd.to_datetime(Unionarchivos['fecha'], format="%Y/%m/%d")
+    raise NotImplementedError("Implementar esta función")
 
-    buscarnulos_media = Unionarchivos.where(Unionarchivos.notna(), Unionarchivos.mean(axis=1, numeric_only=True), axis=0)
- 
-    eliminar_duplicados = buscarnulos_media.drop_duplicates(['fecha'], keep='first')
-
-    convertirfilas_columnas = pd.melt(eliminar_duplicados, id_vars="fecha")
-    ordenar_valores= convertirfilas_columnas.sort_values(by = ['fecha', 'variable'])
-    ordenar_valores.rename(columns={'variable': 'hora', 
-                           'value': 'precio'}, inplace=True)
-
-    ordenar_valores.to_csv('data_lake/cleansed/precios-horarios.csv', encoding='utf-8', index=False)
-
-    #raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
-
     import doctest
-    doctest.testmod()
-
     clean_data()
+    doctest.testmod()
